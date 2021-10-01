c3086dd83a0d455bc95879289158934e61c4e640
Gul-lug <masha.gul15@gmail.com>
2021-10-01 12:25:06 +0300

    working main and added requirements

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.solution import find_duplicates
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-fn', '--file_name', type=str, default="companies.csv",
+                        help='Input name of csv file with data')
+
+    args = parser.parse_args()
+    return args.file_name
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    find_duplicates(main())
--- src/solution.py
+++ src/solution.py
@@ -1,43 +1,24 @@
 import pandas as pd
 import re
 from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-
-df = pd.read_csv("companies.csv", index_col='Id')
-df["name"] = ["".join([j for j in re.split('[?& /|,\.\-\'\"]', i.lower())]) for i in df["name"]]
 
 
-# df["short_name"]=["".join([j for j in i.lower().split()]) for i in df["short_name"]]
-# print(pd.isna(df['short_name']))
-print(len(df[df['short_name'].notnull()]))
-#print(df.index[0])
-print(df.iloc(list(df.index.values).index(120052)))
-print(df.iloc[0])
-def compare_short_names(id_1, id_2):
-    raw_1=df.iloc(list(df.index.values).index(id_1))
-    raw_2=df.iloc(list(df.index.values).index(id_2))
+def data_normalization(file_name: str = "companies.csv") -> pd.core.frame.DataFrame:
+    df = pd.read_csv(file_name)
+    df["name"] = ["".join([j for j in re.split('[?& /|,\.\-\'\"]', i.lower())]) for i in df["name"]]
+    return df
 
-def find_duplicates(df):
+
+def find_duplicates(file_name: str = "companies.csv") -> list:
+    df = data_normalization(file_name)
     s = []
-    for i_id, i in zip(df.index, df["name"]):
-        for j_jd, j in zip(df.index, df["name"]):
+    for i_id, i in zip(df["Id"], df["name"]):
+        for j_jd, j in zip(df["Id"], df["name"]):
             if i_id != j_jd and (j_jd, i_id) not in s:
                 if i == j:
                     s.append((i_id, j_jd))
                 else:
-                    if fuzz.ratio(j, i)>85:
+                    if fuzz.ratio(j, i) > 85:
                         print(i, j, i_id, j_jd)
                         s.append((i_id, j_jd))
-                    '''elif len(i) >= len(j) and j in i:
-                    print(i, j, i_id, j_jd)
-                    s.append((i_id, j_jd))
-                elif len(i) < len(j) and i in j:  # j.find(i,0, len(j)//2)!=-1
-                    print(i, j, i_id, j_jd)
-                    s.append((i_id, j_jd))'''
-
     print(len(s))
-
-
-find_duplicates(df)
-# print(df.duplicated("name"))
-# print(df[df.duplicated(keep=False)])
